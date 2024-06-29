fad4f7ec480e166157aaf5023bf0c9ccb59c90b5
Milos Kaurin <2141359+Kaurin@users.noreply.github.com>
2024-06-29 19:13:27 +0100

    Hostname cosmetics

--- main.tf
+++ main.tf
@@ -55,13 +55,13 @@ module "teleport_lab" {
       ]
     },
     {
-      name     = "kubernetes-1"
+      name     = "kubernetes"
       quantity = 1
       ram      = 4096
       vcpu     = 16
       meta_data = {
-        "instance-id" : "kubernetes-1",
-        "local-hostname" : "kubernetes-1"
+        "instance-id" : "kubernetes",
+        "local-hostname" : "kubernetes"
       }
       user_data = local.user_data
       network_configs = [
@@ -82,17 +82,45 @@ module "teleport_lab" {
       ]
     },
     {
-      name     = "teleport_target"
-      quantity = 3
-      ram      = 1024
+      name     = "teleport-node"
+      quantity = 2
+      ram      = 512
       vcpu     = 1
       meta_data = {
-        "instance-id" : "teleport_target",
-        "local-hostname" : "teleport_target"
+        "instance-id" : "teleport-node",
+        "local-hostname" : "teleport-node"
       }
       user_data = local.user_data
       network_configs = [
-        for num in range(201, 204) : # 201-203, not including 204
+        for num in range(201, 203) : # 201-202, not including 203
+        {
+          "network" : {
+            "version" : 2
+            "ethernets" : {
+              "eth0" : {
+                "addresses" : ["192.168.0.${num}/24"]
+                "gateway4" : "192.168.0.1"
+                "nameservers" : {
+                  "addresses" : ["192.168.0.1", "192.168.0.2"]
+                }
+              }
+            }
+          }
+        }
+      ]
+    },
+    {
+      name     = "teleport-node-agentless"
+      quantity = 2
+      ram      = 512
+      vcpu     = 1
+      meta_data = {
+        "instance-id" : "teleport-node-agentless",
+        "local-hostname" : "teleport-node-agentless"
+      }
+      user_data = local.user_data
+      network_configs = [
+        for num in range(203, 205) : # 203-204, not including 205
         {
           "network" : {
             "version" : 2

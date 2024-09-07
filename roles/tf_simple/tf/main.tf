3617a021ca828b21b45fa964d29ce236809f68fa
Milos Kaurin <2141359+Kaurin@users.noreply.github.com>
2024-09-07 03:17:19 +0100

    No need for two files

--- roles/tf_simple/tf/main.tf
+++ roles/tf_simple/tf/main.tf
@@ -1,3 +1,19 @@
+variable "public_key" {
+  type        = string
+  description = "Location of the public key. For example: `~/.ssh/teleport.pub`"
+}
+
+variable "cloud_image" {
+  type        = string
+  description = "Location of the QCOW2 cloud image. For example: `/home/myuser/Downloads/Fedora-Cloud-Base-Generic.x86_64-40-1.14.qcow2`"
+}
+
+variable "login_username" {
+  type        = string
+  description = "Default login username for the guest virtual machines"
+}
+
+
 locals {
   user_data = {
     "packages" : ["python"]

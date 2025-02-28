401580c20234f55d6c050ed728a5e33bc637c4ab
Milos Kaurin <2141359+Kaurin@users.noreply.github.com>
2025-02-28 22:04:08 +0000

    feat(kubernetes_dynamic): First working version

--- roles/tf_kubernetes_dynamic/tf/main.tf
+++ roles/tf_kubernetes_dynamic/tf/main.tf
@@ -34,7 +34,7 @@ locals {
 module "teleport_lab" {
   # source = "../terraform-libvirt-lab"
   source  = "Kaurin/lab/libvirt"
-  version = "0.2.1"
+  version = "0.3.0"
 
   libvirt_pool_name = "teleport_pool"
   libvirt_pool_dir  = "/var/teleport_pool"
